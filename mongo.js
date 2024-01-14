9f256b15ebfc07875b70d0e5bebeee45a41e3b0e
pullaposki <pullaposki@windowslive.com>
2024-01-14 17:52:55 +0200

    Add MongoDB connection and basic Note model
    This update includes the creation of a new `mongo.js` file which sets up a connection to the MongoDB database and creates a basic model for a Note collection. The model includes `content` and `important` fields. The script also checks for and extracts a password from command line arguments, connects to the database, and saves a new note instance.

---
+++ mongo.js
@@ -0,0 +1,41 @@
+﻿// Import module to interact with MongoDB
+const mongoose = require('mongoose')
+
+// Check for command line argument as password
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+// Extract password from command line argument
+const password = process.argv[2]
+
+// Prepare MongoDB connection string with the password
+const url =
+    `mongodb+srv://test:${password}@testiklusteri.bwokbqd.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+// Disabling strict mode for mongoose to ignore invalid fields in query
+mongoose.set('strictQuery', false)
+// Connecting to MongoDB
+mongoose.connect(url)
+
+// Define schema for Note collection, with string content and boolean importance
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+// Create a Note model using the schema
+const Note = mongoose.model('Note', noteSchema)
+
+// Create and save a new instance of Note, closing the connection afterwards
+const note = new Note({
+  content: 'HTML is Easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log(result)
+  console.log('note saved!')
+  mongoose.connection.close()
+})
