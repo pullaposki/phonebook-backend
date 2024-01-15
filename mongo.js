dcef4c092c779c66474072e3eec92af648dbdd99
pullaposki <pullaposki@windowslive.com>
2024-01-15 17:15:46 +0200

    Refactor mongo.js to add and retrieve persons
    The functionality of mongo.js has been refactored to manage persons instead of notes. Now, the script accepts name and number as command line arguments to create a 'Person' in MongoDB. If these arguments are not supplied, the script fetches and prints all 'Persons' in the database. The operation has been encapsulated into different functions for clean and distinct functionality.

--- mongo.js
+++ mongo.js
@@ -3,39 +3,80 @@ const mongoose = require('mongoose')
 
 // Check for command line argument as password
 if (process.argv.length<3) {
-  console.log('give password as argument')
+  console.log('give password and name as arguments')
   process.exit(1)
 }
 
-// Extract password from command line argument
+// Extract arguments
 const password = process.argv[2]
 
-// Prepare MongoDB connection string with the password
-const url =
-    `mongodb+srv://test:${password}@testiklusteri.bwokbqd.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+const connectToDb = () => {
+  // Prepare MongoDB connection string with the password
+  const url =
+      `mongodb+srv://test:${password}@testiklusteri.bwokbqd.mongodb.net/persons?retryWrites=true&w=majority`
 
 // Disabling strict mode for mongoose to ignore invalid fields in query
-mongoose.set('strictQuery', false)
-// Connecting to MongoDB
-mongoose.connect(url)
+  mongoose.set('strictQuery', false)
 
-// Define schema for Note collection, with string content and boolean importance
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
+// Connecting to MongoDB
+  mongoose.connect(url)
+}
+const saveNewPerson = (name, number) => {
+  // Define schema for Note collection, with string content and boolean importance
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+  })
 
 // Create a Note model using the schema
-const Note = mongoose.model('Note', noteSchema)
+  const Person = mongoose.model('Person', personSchema)
 
 // Create and save a new instance of Note, closing the connection afterwards
-const note = new Note({
-  content: 'HTML is Easy',
-  important: true,
-})
+  const person = new Person({
+    name: name,
+    number: number
+  })
 
-note.save().then(result => {
-  console.log(result)
-  console.log('note saved!')
-  mongoose.connection.close()
-})
+  person.save().then(result => {
+    console.log('person saved!', result)
+    mongoose.connection.close()
+  })
+}
+const printPersons = () => {
+  // Define schema for Note collection, with string content and boolean importance
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+  })
+
+// Create a Note model using the schema
+  const Person = mongoose.model('Person', personSchema)
+
+  Person.find({})
+      .then(result => {
+        console.log("phonebook:")
+        result.forEach(person => {
+          console.log(person.name +" "+ person.number)
+        })
+      })
+        mongoose.connection.close()
+      .then(()=>{
+      })
+}
+
+if (process.argv.length > 3){
+  console.log(process.argv)
+  const name = process.argv[3]
+
+  let number=0
+  if (process.argv.length > 3) {
+    number = process.argv[4]
+
+  }
+  connectToDb()
+  saveNewPerson(name, number)
+}else {
+  connectToDb()
+  printPersons()
+}
